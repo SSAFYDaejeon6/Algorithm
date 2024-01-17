43b7381177179bdbf7e49cee4d3f5a63101ac411
Dongjun_Lee <110724067+djleeasi@users.noreply.github.com>
2024-01-17 13:34:18 +0900

    Create 1330.java

---
+++ 이동준/backjoon/1330.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Main{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		
+		String out = "";
+		if(A == B)
+			out = "==";
+		if(A > B)
+			out = ">";
+		if(A < B)
+			out = "<";
+		System.out.println(out);
+		sc.close();
+	}
+}
